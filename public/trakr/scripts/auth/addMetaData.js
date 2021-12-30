967fe30c2997d630b40fc31d0ac441533977f066
dannycdannydo <51877703+dannycdannydo@users.noreply.github.com>
2021-12-30 23:38:33 +0000

    commit

---
+++ public/trakr/scripts/auth/addMetaData.js
@@ -0,0 +1,22 @@
+var axios = require("axios").default;
+
+let addMetaData = async function addMetaData(data)
+{
+  return new Promise(async function(resolve, reject)
+  {
+    var options = {
+        method: 'PATCH',
+        url: 'https://dev--gfk8fe3.eu.auth0.com/api/v2/users/user_id',
+        headers: {authorization: 'Bearer ABCD', 'content-type': 'application/json'},
+        data: {user_metadata: {customer: req.body}}
+      };
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        resolve(response.data)
+      }).catch(function (error) {
+        console.error(error);
+      });
+    })
+}
+
+module.exports.addMetaData = addMetaData
--- routes/trakr/auth/addMetaData.js
+++ routes/trakr/auth/addMetaData.js
@@ -1,20 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var axios = require("axios").default;
+const { addMetaData } = require('../../../public/trakr/scripts/auth/addMetaData')
 
 
 router.post('/trakr/auth/addMetaData', async function(req, res, next) {
-    var options = {
-        method: 'PATCH',
-        url: 'https://dev--gfk8fe3.eu.auth0.com/api/v2/users/user_id',
-        headers: {authorization: 'Bearer ABCD', 'content-type': 'application/json'},
-        data: {user_metadata: {customer: req.body}}
-      };
-      axios.request(options).then(function (response) {
-        console.log(response.data);
-      }).catch(function (error) {
-        console.error(error);
-      });
+    const result = await addMetaData(req.body)
+    return result
 });
 
 module.exports = router;
