93db1ac34c29d735b18df82bee07486654a56f57
dannycdannydo <51877703+dannycdannydo@users.noreply.github.com>
2021-12-31 00:13:45 +0000

    commit

--- routes/trakr/auth/newUser.js
+++ routes/trakr/auth/newUser.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { createCustomer } = require('../../../public/trakr/scripts/chargeBee/createCustomer')
-const { addMetaData } = require('../../../public/trakr/scripts/auth/addMetaData')
+const { updateMetaData } = require('../../../public/trakr/scripts/auth/updateMetaData')
 
 
 
@@ -10,7 +10,7 @@ router.post('/trakr/auth/newUser', async function(req, res, next) {
     user.email = req.body.params.email.email
     user.user_id = req.body.params.email.user_id
     const result = await createCustomer(user)
-    await addMetaData(result, user.user_id)
+    await updateMetaData(result, user.user_id)
     res.send(result)
 });
 
--- routes/trakr/auth/updateMetaData.js
+++ routes/trakr/auth/updateMetaData.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var axios = require("axios").default;
-const { updateMetaData } = require('../../../public/trakr/scripts/auth/addMetaData')
+const { updateMetaData } = require('../../../public/trakr/scripts/auth/updateMetaData')
 
 
-router.post('/trakr/auth/addMetaData', async function(req, res, next) {
+router.post('/trakr/auth/updateMetaData', async function(req, res, next) {
     const result = await updateMetaData(req.body, req.body.userId)
     return result
 });
