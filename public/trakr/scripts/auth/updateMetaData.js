8ba7268d30672a7662c667dff06ad4b0cb750def
dannycdannydo <51877703+dannycdannydo@users.noreply.github.com>
2021-12-31 00:24:21 +0000

    commit

--- public/trakr/scripts/auth/updateMetaData.js
+++ public/trakr/scripts/auth/updateMetaData.js
@@ -1,13 +1,15 @@
 var axios = require("axios").default;
+const { getToken } = require('./authManage')
 
 let updateMetaData = async function updateMetaData(data, userId)
 {
   return new Promise(async function(resolve, reject)
   {
+    const token = getToken()
     var options = {
         method: 'PATCH',
         url: 'https://dev--gfk8fe3.eu.auth0.com/api/v2/users/' + userId,
-        headers: {authorization: 'Bearer ABCD', 'content-type': 'application/json'},
+        headers: {authorization: `${token.token_type} ${token.access_token}`, 'content-type': 'application/json'},
         data: {user_metadata: {customer: data.customer}}
       };
       axios.request(options).then(function (response) {
