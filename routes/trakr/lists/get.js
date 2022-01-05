c6417c0131fd55774e0bbfb1227809a7197230a5
dannycdannydo <51877703+dannycdannydo@users.noreply.github.com>
2022-01-05 23:30:02 +0000

    commit

--- routes/trakr/lists/get.js
+++ routes/trakr/lists/get.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 router.post('/trakr/lists/get', async function(req, res, next) {
   try {
-    console.log(req.body)
     const list = require(`../../../public/trakr/lists/${req.body.list}.js`)
     res.send(list[req.body.list])
   } catch {
