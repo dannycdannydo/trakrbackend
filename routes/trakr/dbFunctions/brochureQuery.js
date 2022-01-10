e949ac9bcd333690b493a3a1458f8907388b51b5
Daniel Campbell <daniel.campbell90@gmail.com>
2022-01-10 16:32:57 +0000

    commit

--- routes/trakr/dbFunctions/brochureQuery.js
+++ routes/trakr/dbFunctions/brochureQuery.js
@@ -4,6 +4,7 @@ const { brochureQueryMongoliser, mongoQuery, mongoSums } = require('../../../pub
 const _ = require('lodash')
 
 router.post('/trakr/dbFunctions/brochureQuery', async function(req, res, next) {
+    console.log(req.body)
     const exclude = []
     if (req.body.exclude) {
         for (var e in req.body.exclude) {
