17b1977e7ab1574787c698c7c09bd5e876c8d78c
dannycdannydo <51877703+dannycdannydo@users.noreply.github.com>
2022-01-11 22:01:51 +0000

    commit

---
+++ config/env/production.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trakrEmailPword : process.env.trakrEmailPword,
+    authClientId: process.env.authClientId,
+    authClientSecret: process.env.authClientSecret,
+    trakrDBConnectionString: process.env.trakrDBConnectionString,
+    azureStorageConnectionString: process.env.azureStorageConnectionString,
+    chargebeeSiteTest: process.env.chargebeeSiteTest,
+    chargeBeeApiKeyTest: process.env.chargeBeeApiKeyTest,
+    chargebeeSite: process.env.chargebeeSite,
+    chargeBeeApiKey: process.env.chargeBeeApiKey,
+    gmapApiKey: process.env.gmapApiKey,
+}
