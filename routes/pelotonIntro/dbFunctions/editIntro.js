77881d106d4f8544c15b5fa8afc3d44cce4f77c3
dannycdannydo <51877703+dannycdannydo@users.noreply.github.com>
2022-02-28 22:47:26 +0000

    add attachments

--- routes/pelotonIntro/dbFunctions/editIntro.js
+++ routes/pelotonIntro/dbFunctions/editIntro.js
@@ -6,6 +6,9 @@ const dates = ['dateSent']
 var multer  = require('multer')
 var upload = multer({ })
 const { getAttachmentNames } = require('../../../public/pelotonIntro/scripts/analysis/emailAnalysis')
+const { uploadFile } = require('../../../public/pelotonIntro/scripts/storage/uploadFile')
+const { pdfToJpg } = require('../../../public/pelotonIntro/scripts/pdfWork/pdfToJpg')
+var mime = require('mime-types')
 
 router.post('/pelotonIntro/dbFunctions/editIntro', async function(req, res, next) {
   const query = {}
@@ -29,32 +32,38 @@ router.post('/pelotonIntro/dbFunctions/editIntro', async function(req, res, next
     }
   }
   const result = await mongoUpdate('peloton', 'intros', req.body.filter, query)
-  res.send('hello')
+  res.send(result)
 });
 
 router.post('/pelotonIntro/dbFunctions/editIntroFile', upload.single('file'), async function(req, res, next) {
   const file = req.file
-  const data = JSON.parse(req.body.data)
-    // const query = {}
-    // if (req.body.data) {
-    //   for (const [key] of Object.entries(req.body.data)) {
-    //     for (let [k, v] of Object.entries(req.body.data[key])) {
-    //       if (numbers.includes(k)) {
-    //         v = v.replace(/[^0-9\.]/g,'');
-    //         v = v * 1
-    //       }
-    //       if (dates.includes(k)) {
-    //         try {
-    //           v = new Date(v)
-    //         } catch {}
-    //       }
-    //       query[`${key}.${k}`] = v
-    //     }
-    //   }
-    // }
-    // const result = await mongoUpdate('peloton', 'intros', req.body.filter, query)
-    res.send('hello')
-  });
+  req.body.data = JSON.parse(req.body.data)
+  req.body.filter = JSON.parse(req.body.filter)
+  const query = {}
+  if (req.body.data) {
+    for (const [key] of Object.entries(req.body.data)) {
+      for (let [k, v] of Object.entries(req.body.data[key])) {
+        if (numbers.includes(k)) {
+          v = v.replace(/[^0-9\.]/g,'');
+          v = v * 1
+        }
+        if (dates.includes(k)) {
+          try {
+            v = new Date(v)
+          } catch {}
+        }
+        query[`${key}.${k}`] = v
+      }
+    }
+  }
+  const result = await mongoUpdate('peloton', 'intros', req.body.filter, query)
+  uploadFile(file.buffer, req.body.fileName, 'peloton', "." + mime.extension(file.mimetype))
+  if (mime.extension(file.mimetype).includes("pdf")) {
+      const img = await pdfToJpg(file.buffer, req.body.fileName)
+      uploadFile(img, req.body.fileName, 'peloton', ".jpg")
+  }
+  res.send(result)
+});
 
 
 module.exports = router;
