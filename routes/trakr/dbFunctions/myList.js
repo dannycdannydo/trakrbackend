32a6589916cf5a964f4be82b9bcf694d0c46a4f3
dannycdannydo <51877703+dannycdannydo@users.noreply.github.com>
2022-04-04 22:33:38 +0100

    commit

--- routes/trakr/dbFunctions/myList.js
+++ routes/trakr/dbFunctions/myList.js
@@ -8,37 +8,68 @@ router.post('/trakr/dbFunctions/addToMyList', async function(req, res, next) {
 });
 
 router.post('/trakr/dbFunctions/checkMyList', async function(req, res, next) {
-    try {
-        const result = await mongoQuery('userLists', req.body.user, {'base.filename': req.body.asset.base.filename}, 100000000, {'meta.dateCreated': -1})
-        if (result[0]) {
-            res.send(true)
-        } else {
+    if (req.body.user) {
+        try {
+            const result = await mongoQuery('userLists', req.body.user, {'base.filename': req.body.asset.base.filename}, 100000000, {'meta.dateCreated': -1})
+            if (result[0]) {
+                res.send(true)
+            } else {
+                res.send(false)
+            }
+        } catch {
             res.send(false)
         }
-    } catch {
+    } else {
+        res.send(false)
+    }
+    
+});
+
+router.post('/trakr/dbFunctions/removeFromMyList', async function(req, res, next) {
+    if (req.body.user) {
+        try {
+            const result = await mongoDelete('userLists', req.body.user, {'base.filename': req.body.asset.base.filename})
+            res.send(result)
+        } catch {
+            res.send(false)
+        }
+    }
+});
+
+router.post('/trakr/dbFunctions/clearMyList', async function(req, res, next) {
+    if (req.body.user) {
+        try {
+            const result = await mongoDeleteMany('userLists', req.body.user, {})
+            res.send(result)
+        } catch {
+            res.send(false)
+        }
+    } else {
         res.send(false)
     }
 });
 
-router.post('/trakr/dbFunctions/removeFromMyList', async function(req, res, next) {
-    const result = await mongoDelete('userLists', req.body.user, {'base.filename': req.body.asset.base.filename})
-    res.send(result)
-});
-
-router.post('/trakr/dbFunctions/clearMyList', async function(req, res, next) {
-    const result = await mongoDeleteMany('userLists', req.body.user, {})
-    res.send(result)
-});
-
 router.post('/trakr/dbFunctions/getMyList', async function(req, res, next) {
-    const result = await mongoQuery('userLists', req.body.user, {}, 100000000, {'meta.dateCreated': -1})
-    res.send(result)
+    if (req.body.user) {
+        try {
+            const result = await mongoQuery('userLists', req.body.user, {}, 100000000, {'meta.dateCreated': -1})
+            res.send(result)
+        } catch {
+            res.send(false)
+        }
+    } else {
+        res.send(false)
+    }
 });
 
 router.post('/trakr/dbFunctions/countMyList', async function(req, res, next) {
     if (req.body.user) {
-        const result = await mongoCount('userLists', req.body.user, {})
-        res.send({value: result})
+        try {
+            const result = await mongoCount('userLists', req.body.user, {})
+            res.send({value: result})
+        } catch {
+            res.send(false)
+        }
     } else {
         res.send({value: 0})
     }
