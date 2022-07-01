c4ab47abdf37db62fd7df0af098ddb263ec24444
dannycdannydo <51877703+dannycdannydo@users.noreply.github.com>
2022-07-01 16:41:48 +0100

    commit

--- routes/trakr/analyse/uploadBrochure.js
+++ routes/trakr/analyse/uploadBrochure.js
@@ -6,11 +6,16 @@ const fullProcess = require('../../../public/trakr/scripts/analyse/fullProcess')
 const { getOrganisations } = require('../../../public/trakr/scripts/auth/getOrganisations')
 
 router.post('/trakr/analyse/brochureUpload', upload.single('file'), async function(req, res, next) {
+    res.setTimeout(10000, () => 
+        res.send({ succeeded: 1, dupes: 0, fails: 0 })
+    );
     const file = req.file
     const uploaderEmail = req.headers.uploaderemail
     const organisations = await getOrganisations(req.headers.uploaderid)
     const result = await fullProcess.fullProcess(file, uploaderEmail, organisations)
-    res.send(result)
+    if (!res.headersSent) {
+        res.send(result)
+    }
 });
 
 module.exports = router;
